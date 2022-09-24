83acd05d70f97059d88bc89fdac42cbab1cc09d5
Joao Victor <joaovictordeveloper@outlook.com>
2022-09-24 03:03:28 -0300

    feat: criando arquivo de tema

---
+++ src/styles/theme.js
@@ -0,0 +1,16 @@
+const colors = {
+  primary: 'black',
+  primary_200: 'rgba(0,0,0, 0.2)',
+  primary_500: 'rgba(0,0,0, 0.5)',
+
+  secondary: 'white',
+  secondary_500: 'rgba(255, 255, 255, 0.5)',
+};
+
+const fonts = {
+  textLogo: 24,
+  title: 16,
+  text: 14,
+};
+
+export { colors, fonts };
