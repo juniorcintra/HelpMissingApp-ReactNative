819e490684346fefba4ddecb380ab2372f419e6c
Joao Victor <joaovictordeveloper@outlook.com>
2022-09-28 01:27:28 -0300

    fix: centralizando o header title da home

--- src/routes/MainDrawer.js
+++ src/routes/MainDrawer.js
@@ -7,7 +7,7 @@ const { Navigator, Screen } = createDrawerNavigator();
 const MainDrawer = () => {
   return (
     <Navigator initialRouteName='Home'>
-      <Screen name='Home' component={HomePage} />
+      <Screen name='Home' component={HomePage} options={{headerTitleAlign: 'center'}} />
     </Navigator>
   );
 };
