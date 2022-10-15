0b34b00b9ab99a6b5f94ca4039f4a783388120a1
Joao Victor <joaovictordeveloper@outlook.com>
2022-10-15 10:39:46 -0300

    fix: ajustando o component modal no loading

--- src/components/loading.js
+++ src/components/loading.js
@@ -1,21 +1,12 @@
 import React from 'react';
-import { Modal, ActivityIndicator, View } from 'react-native';
-import { colors } from '../styles/theme';
+import { ActivityIndicator } from 'react-native';
+
+import Modal from '../components/Modal';
 
 export default ({ show }) => {
   return (
-    <Modal animationType='fade' transparent={true} visible={show}>
-      <View
-        style={{
-          height: '100%',
-          width: '100%',
-          flex: 1,
-          backgroundColor: colors.primary_500,
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
-        <ActivityIndicator size='large' color='#FA472B' animating={true} />
-      </View>
+    <Modal visible={show}>
+      <ActivityIndicator size='large' color='#FA472B' animating={true} />
     </Modal>
   );
 };
