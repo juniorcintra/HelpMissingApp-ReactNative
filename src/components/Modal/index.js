e8c4ee1f460141076dcad805a821cf0229075c3b
Joao Victor <joaovictordeveloper@outlook.com>
2022-10-20 22:24:26 -0300

    fix: adicionando botão de fechar no botão

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import { Modal, View, StyleSheet } from 'react-native';
+import { Modal, View, StyleSheet, Image, TouchableOpacity } from 'react-native';
 
 import { colors } from '../../styles/theme';
 
+const iconClose = require('../../../assets/images/close_icon.png');
+
 export default props => {
   return (
-    <Modal animationType='fade' transparent={true} visible={props.show} onRequestClose={() => {
-      props.setShowModal(false);
-    }}{...props} >
-      <View style={styles.container}>{props.children}</View>
+    <Modal
+      animationType='fade'
+      transparent={true}
+      visible={props.show}
+      onRequestClose={() => props.setShowModal(false)}
+      {...props}
+    >
+      <View style={styles.container}>
+        <TouchableOpacity activeOpacity={0.6} onPress={() => props?.setShowModal(false)}>
+          <Image source={iconClose} style={styles.iconClose} />
+        </TouchableOpacity>
+        {props.children}
+      </View>
     </Modal>
   );
 };
@@ -20,5 +31,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: colors.primary_500,
+  },
+  iconClose: {
+    height: 35,
+    width: 35,
+    marginBottom: -12,
+    marginLeft: 305
   },
 });
