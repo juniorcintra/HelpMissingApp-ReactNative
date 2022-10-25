d59a84682075cb0d1acc4cf43831d8c1052baf58
Joao Victor <joaovictordeveloper@outlook.com>
2022-10-24 23:30:41 -0300

    feat: criando scroll de pessoas cadastradas

--- src/pages/PerfilPage/index.js
+++ src/pages/PerfilPage/index.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { Image, ImageBackground, Text, TextInput, View } from 'react-native';
+import { Image, ImageBackground, ScrollView, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import Modal from '../../components/Modal';
+import Button from '../../components/Button';
 
 import styles from './styles';
 
@@ -17,8 +20,27 @@ const PerfilPage = () => {
   const [emailUser, setEmailUser] = useState('');
   const [loginUser, setLoginUser] = useState('');
   const [contatoUser, setContatoUser] = useState('');
+  const [photosSelected, setPhotosSelected] = useState('');
 
   const [editable, setEditable] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+
+  const dateScroll = [
+    { uri: 'https://wpblog.zyro.com/cdn-cgi/image/w=1200,q=85/wp-content/uploads/2021/01/exemples-sites-photod.jpg' },
+    {
+      uri: 'https://wpblog.zyro.com/cdn-cgi/image/w=1200,q=85/wp-content/uploads/2022/04/person-holding-iphone-instagram-open.jpg',
+    },
+    {
+      uri: 'https://wpblog.zyro.com/cdn-cgi/image/w=1200,q=85/wp-content/uploads/2022/03/colorful-shirts-on-clothing-racks-indoors.jpg',
+    },
+    { uri: 'https://wpblog.zyro.com/cdn-cgi/image/w=1200,q=85/wp-content/uploads/2021/01/exemples-sites-photod.jpg' },
+    {
+      uri: 'https://wpblog.zyro.com/cdn-cgi/image/w=1200,q=85/wp-content/uploads/2022/04/person-holding-iphone-instagram-open.jpg',
+    },
+    {
+      uri: 'https://wpblog.zyro.com/cdn-cgi/image/w=1200,q=85/wp-content/uploads/2022/03/colorful-shirts-on-clothing-racks-indoors.jpg',
+    },
+  ];
 
   const getUser = async () => {
     const asyncStrg = await AsyncStorage.getItem('user');
@@ -32,52 +54,78 @@ const PerfilPage = () => {
     }
   };
 
+  const handleOpenModal = photo => {
+    setPhotosSelected(photo);
+    setShowModal(true);
+  };
+
   useEffect(() => {
     getUser();
   }, [user]);
 
   return (
-    <View style={styles.container}>
-      <ImageBackground source={imageBG} style={styles.header}>
-        <Image source={imageAvatar} style={styles.headerAvatar} />
-        <Text style={styles.headerTitle}>{loginUser}</Text>
-      </ImageBackground>
+    <>
+      <ScrollView>
+        <View style={styles.container}>
+          <ImageBackground source={imageBG} style={styles.header}>
+            <Image source={imageAvatar} style={styles.headerAvatar} />
+            <Text style={styles.headerTitle}>{loginUser}</Text>
+          </ImageBackground>
 
-      <View style={styles.divInput}>
-        <Text style={styles.label}>Nome Completo</Text>
-        <TextInput value={nameUser} editable={editable} style={styles.input} onChangeText={setNameUser} />
-      </View>
-      <View style={styles.divInput}>
-        <Text style={styles.label}>CPF</Text>
-        <TextInput
-          value={cpfUser}
-          editable={false}
-          style={styles.input}
-          keyboardType='number-pad'
-          onChangeText={setCpfUser}
-        />
-      </View>
-      <View style={styles.divInput}>
-        <Text style={styles.label}>E-mail</Text>
-        <TextInput
-          value={emailUser}
-          editable={editable}
-          style={styles.input}
-          onChangeText={setEmailUser}
-          keyboardType='email-address'
-        />
-      </View>
-      <View style={styles.divInput}>
-        <Text style={styles.label}>Contato</Text>
-        <TextInput
-          editable={editable}
-          value={contatoUser}
-          style={styles.input}
-          keyboardType='number-pad'
-          onChangeText={setContatoUser}
-        />
-      </View>
-    </View>
+          <View style={styles.divInput}>
+            <Text style={styles.label}>Nome Completo</Text>
+            <TextInput value={nameUser} editable={editable} style={styles.input} onChangeText={setNameUser} />
+          </View>
+          <View style={styles.divInput}>
+            <Text style={styles.label}>CPF</Text>
+            <TextInput
+              value={cpfUser}
+              editable={false}
+              style={styles.input}
+              keyboardType='number-pad'
+              onChangeText={setCpfUser}
+            />
+          </View>
+          <View style={styles.divInput}>
+            <Text style={styles.label}>E-mail</Text>
+            <TextInput
+              value={emailUser}
+              editable={editable}
+              style={styles.input}
+              onChangeText={setEmailUser}
+              keyboardType='email-address'
+            />
+          </View>
+          <View style={styles.divInput}>
+            <Text style={styles.label}>Contato</Text>
+            <TextInput
+              editable={editable}
+              value={contatoUser}
+              style={styles.input}
+              keyboardType='number-pad'
+              onChangeText={setContatoUser}
+            />
+          </View>
+          <View style={styles.divScroll}>
+            <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+              {dateScroll.map((item, index) => (
+                <TouchableOpacity key={index} activeOpacity={0.6} onPress={() => handleOpenModal(item.uri)}>
+                  <Image source={{ uri: item.uri }} style={styles.photoScroll} />
+                </TouchableOpacity>
+              ))}
+            </ScrollView>
+          </View>
+        </View>
+      </ScrollView>
+
+      {/* Modal para visualizar foto */}
+      <Modal show={showModal} setShowModal={setShowModal}>
+        <View style={[styles.contentModal, { paddingBottom: 25 }]}>
+          <Image source={{ uri: photosSelected }} style={styles.modalPhoto} />
+          <Button title="Já foi encontrado!" />
+        </View>
+      </Modal>
+    </>
   );
 };
 
--- src/pages/PerfilPage/styles.js
+++ src/pages/PerfilPage/styles.js
@@ -45,6 +45,39 @@ const styles = StyleSheet.create({
   lastInput: {
     marginBottom: 40,
   },
+  divScroll: {
+    marginTop: 8,
+    marginBottom: 15,
+    marginHorizontal: 8,
+  },
+  photoScroll: {
+    width: 120,
+    height: 120,
+    marginHorizontal: 4,
+    borderRadius: 10
+  },
+  contentModal: {
+    width: '80%',
+    height: 493,
+    borderRadius: 15,
+    paddingHorizontal: 25,
+    paddingTop: 32,
+    paddingBottom: 13,
+    backgroundColor: colors.white,
+  },
+  modalPhoto: {
+    width: '100%',
+    height: '80%',
+    borderRadius: 5,
+    marginBottom: 8,
+    resizeMode: 'cover',
+  },
+  itleModalHistory: {
+    fontSize: 20,
+    fontFamily: 'Roboto-Bold',
+    textAlign: 'center',
+    color: '#000',
+  },
 });
 
 export default styles;
