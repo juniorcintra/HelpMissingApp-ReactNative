a92db572289958c5cce9f47dd52604c35179682e
juniorcintra <junior.cintra.developer@gmail.com>
2022-10-25 13:06:35 -0300

    fix: :bug: correcao pessoas encontradas

--- src/pages/PeopleFound/index.js
+++ src/pages/PeopleFound/index.js
@@ -16,22 +16,45 @@ const PeopleFound = ({ navigation }) => {
   const { missingPersons } = useSelector(state => state.missingPersonReducer);
   const { user } = useSelector(state => state.userReducer);
 
-
   const handleInfo = () => {};
 
-  const handleGetMissingPersons = async name => {
-    if (name.langht > 0) {
-      await dispatch(
-        getMissingPerson(`?limite=1&tem_historico=0&usuarios_id=${user?.id}&encontrado=1&nome=${name}`, true),
-      );
-    } else {
-      await dispatch(getMissingPerson(`?limite=1&tem_historico=0&usuarios_id=${user?.id}&encontrado=1`, true));
-    }
+  const handleGetMissingPersons = async () => {
+    await dispatch(getMissingPerson(`?limite=1&tem_historico=0&usuarios_id=${user?.id}&encontrado=1`, true));
   };
+
+  let listaFiltrada = missingPersons.filter(function (value) {
+    function removerAcentos(newStringComAcento) {
+      var string = newStringComAcento;
+      var mapaAcentosHex = {
+        a: /[\xE0-\xE6]/g,
+        e: /[\xE8-\xEB]/g,
+        i: /[\xEC-\xEF]/g,
+        o: /[\xF2-\xF6]/g,
+        u: /[\xF9-\xFC]/g,
+        c: /\xE7/g,
+        n: /\xF1/g,
+      };
+
+      for (var letra in mapaAcentosHex) {
+        var expressaoRegular = mapaAcentosHex[letra];
+        string = string.replace(expressaoRegular, letra);
+      }
+
+      return string;
+    }
+
+    let semAcento = removerAcentos(value.nome);
+
+    if (search.match(/^.*[^a-zA-Z 0-9]+.*$/g)) {
+      return value.nome.toUpperCase().includes(search.toUpperCase());
+    } else {
+      return semAcento.toUpperCase().includes(search.toUpperCase());
+    }
+  });
 
   useFocusEffect(
     useCallback(() => {
-      handleGetMissingPersons(search);
+      handleGetMissingPersons();
     }, [search]),
   );
 
@@ -63,7 +86,7 @@ const PeopleFound = ({ navigation }) => {
       </View>
 
       <FlatList
-        data={missingPersons}
+        data={listaFiltrada}
         keyExtractor={item => item.id}
         renderItem={renderCard}
         showsVerticalScrollIndicator={false}
